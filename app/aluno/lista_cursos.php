7bbddca62858febcf7dc379cf7dbca93c0570dd6
Victor <viktao@msn.com>
2012-05-08 12:38:57 -0300

    Manual de instalação para o Sistema Operacional Windows, uso exclusivo para testes e desenvolvimento. Alterações na Interface Aluno lista_notas e lista_notas_detalhe
    Lista_Notas:
    Data e Hora
    % Faltas
    (*) Situação
    
    Lista_Notas_Detalhe:
    Data e hora
    Separação de dados aluno e turma
    (*) Situação
    
    Signed-off-by: Victor <viktao@msn.com>

--- app/aluno/lista_cursos.php
+++ app/aluno/lista_cursos.php
@@ -1,13 +1,11 @@
-<?php
+﻿<?php
 
 require_once('aluno.conf.php');
 include_once('includes/topo.htm');
-
-
 // Recupera a lista de cursos e periodos atuais para o aluno
 $sql_curso = '
 SELECT DISTINCT
-    a.ref_curso, e.abreviatura, a.ref_periodo, d.descricao
+    a.ref_curso, e.descricao AS descricao_curso, a.ref_periodo, d.descricao, f.prontuario
 FROM 
     matricula a, pessoas b, disciplinas c,
     periodos d, cursos e, contratos f
@@ -38,14 +36,13 @@ $arr_curso = $conn->get_all(sprintf($sql_curso,$aluno, $data, $aluno));
 
 $sql_aluno = "
 SELECT
-    p.nome, rua, complemento, bairro, p.cep,
+    p.nome, cod_cpf_cgc, rua, complemento, bairro, p.cep,
     c.nome || ' - ' || ref_estado AS cidade,
     fone_particular, fone_profissional, fone_celular,
     fone_recado, email, dt_nascimento
-FROM pessoas p, cidade c
+FROM pessoas p LEFT OUTER JOIN cidade c ON (p.ref_cidade = c.id)
 WHERE 
-    p.id = $aluno AND
-    p.ref_cidade = c.id ;";
+    p.id = $aluno;";
 
 $dados_aluno = $conn->get_all($sql_aluno);
 $arr_aluno = $dados_aluno['0'];
@@ -53,37 +50,24 @@ $arr_aluno = $dados_aluno['0'];
 $curso = '';
 ?>
 <h2>Minhas informa&ccedil;&otilde;es</h2>
-<strong>Registro: </strong>
-<?=str_pad($aluno, 5, "0", STR_PAD_LEFT)?>
-&nbsp;&nbsp;
 <strong>Nome: </strong>
 <?=$arr_aluno['nome']?>
-&nbsp;&nbsp;
+<br />
+<!--
+<strong>CPF: </strong>
+</?=$arr_aluno['cod_cpf_cgc']?>
+<br />-->
 <strong>Data de nascimento:  </strong>
 <?=date::convert_date($arr_aluno['dt_nascimento'])?>
 <br />
-<strong>Endere&ccedil;o: </strong>
-<?=$arr_aluno['rua']?>
-&nbsp;&nbsp;
-<strong>Bairro: </strong>
-<?=$arr_aluno['bairro']?>
-&nbsp;&nbsp;
-<strong>CEP: </strong>
-<?=$arr_aluno['cep']?>
-&nbsp;&nbsp;
-<strong>Cidade: </strong>
-<?=$arr_aluno['cidade']?>
-<br />
-<strong>Telefone: </strong>
-<?=$arr_aluno['fone_particular']?>&nbsp;&nbsp;
 <strong>E-mail:  </strong>
 <?=$arr_aluno['email']?>
 <br />
+<!--
 <font color="red">
     <strong>Aten&ccedil;&atilde;o:</strong>
-    Para corrigir ou atualizar seus dados procure o setor de
-    registros escolares.
-</font>
+    Para corrigir ou atualizar seus dados procure a secretaria escolar.
+</font>-->
 <h2>Meus cursos</h2>
 <table>
     <?php
@@ -95,7 +79,8 @@ $curso = '';
 
     for($i = 0; $i < count($arr_curso) ; $i++) {
         echo '<tr><td><b>';
-        echo $arr_curso[$i]["abreviatura"].'</b><br> |  <a href=lista_notas.php?c='.$arr_curso[$i]["ref_curso"].'&p='.$arr_curso[$i]["ref_periodo"].'>'.$arr_curso[$i]["descricao"].'</a>';
+        echo 'Prontu&aacute;rio: </b>'. $arr_curso[$i]['prontuario'] .'<b><br />';
+        echo $arr_curso[$i]['descricao_curso'].'</b><br> |  <a href=lista_notas.php?c='.$arr_curso[$i]["ref_curso"].'&p='.$arr_curso[$i]["ref_periodo"].'>'.$arr_curso[$i]["descricao"].'</a>';
 
         if ($arr_curso[$i]["ref_curso"] == $arr_curso[$i + 1]["ref_curso"] ) {
             echo ' |   <a href=lista_notas.php?c='.$arr_curso[$i]["ref_curso"].'&p='.$arr_curso[$i + 1]["ref_periodo"].'>'.$arr_curso[$i + 1]["descricao"].'</a> | <br />';
--- app/aluno/lista_notas.php
+++ app/aluno/lista_notas.php
@@ -1,3 +1,4 @@
+﻿<meta http-equiv="Content-Type" content="charset=UTF-8"> </meta>
 <?php
 
 require_once('aluno.conf.php');
@@ -6,7 +7,6 @@ include_once('includes/topo.htm');
 $aluno   = $user;
 $periodo = $_GET["p"];
 $curso   = $_GET["c"];
-
 $rs_pessoa   = $conn->get_one("SELECT nome FROM pessoas WHERE id = $aluno");
 $rs_curso    = $conn->get_one("SELECT descricao FROM cursos WHERE id = $curso");
 $rs_periodo  = $conn->get_one("SELECT descricao FROM periodos WHERE id = '$periodo'");
@@ -35,20 +35,24 @@ $rs_diarios_matriculados = count($rs_diarios);
 <p>
     <strong>Aluno: </strong><?=$aluno?> - <?=$rs_pessoa?><br />
     <strong>Curso: </strong><?=$curso?> - <?=$rs_curso?><br />
-    <strong>Per&iacute;odo: </strong><?=$rs_periodo?>
+    <strong>Perí­odo: </strong><?=$rs_periodo?><br />
+    Gerado às <?php echo date("H:i:s") ." do dia ". date("d/m/y"); ?>
 </p>
-<span style="color: red; font-style:italic; font-family:arial,times;">Clique no nome da disciplina para detalhar os lan&ccedil;amentos e visualizar mais informa&ccedil;&otilde;es</span>
-<table>
-    <tr bgcolor="#545443">
-        <th><font color="#ffffff">Disciplina</font></th>
-        <th><font color="#ffffff">Nota Total</font></th>
-        <th><font color="#ffffff">Nota distribuida</font></th>
-        <th><font color="#ffffff">Faltas</font></th>
-        <th><font color="#ffffff">Situa&ccedil;&atilde;o</font></th>
+<span style="color: red; font-style:italic; font-family:arial,times;">Clique no 
+nome da disciplina para detalhar os lançamentos e visualizar mais informações</span>
+<table style="width: 460px; font-size:12px">
+    <tr bgcolor=#EEEEEE>
+        <th>Disciplina</th>
+        <th>Média</th>
+        <th>Nota<br />Máxima</th>
+        <th>Faltas</th>
+  		<!-- Inicio: Victor Ullisses Pugliese - 12:38 27/04/2012 - % FALTAS; -->
+        <th>% Faltas</th>
+        <th>Situação</th>
     </tr>
     <?php
     $count = 0;
-
+	$m = 0;
         // Exibe as principais informacoes do aluno a.ref_disciplina_ofer IN ($str_in) AND
         $sql_diarios_info = "
         SELECT
@@ -67,28 +71,52 @@ $rs_diarios_matriculados = count($rs_diarios);
         
         //die($sql_diarios_info);
         $diarios_info = $conn->get_all($sql_diarios_info);
-
+		
         if (count($diarios_info) > 0 ) {
             foreach ($diarios_info as $disciplina_aluno) {
 				$nao_finalizada = ($disciplina_aluno['fl_finalizada'] == 'f') ? '<strong>*</strong>' : ' ';
-				$color =  ($color != '#ffffff') ? '#ffffff' : '#cce5ff';
+				$color =  ($color != '#ffffff') ? '#ffffff' : '#A7E6FE';
 
-                $situacao = '';
-
-                if(verificaAprovacao($aluno, $curso, $disciplina_aluno['ref_disciplina_ofer']))
-                    $situacao = 'A';
-                else
-                    $situacao = '<span style="color: red; font-weight: bold;">R</span>';
-
-    			if(!verificaPeriodo($periodo) && $disciplina_aluno['fl_finalizada'] == 'f')
-        			$situacao = 'M';
-
-				echo '<tr bgcolor="'. $color .'">';
-				echo '<td><a href="lista_notas_detalhe.php?c='. $curso .'&p='. $periodo .'&d='. $disciplina_aluno['ref_disciplina_ofer'] .'" alt="Clique para detalhar a disciplina" title="Clique para detalhar a disciplina">'. $disciplina_aluno['descricao_disciplina'] .'</a>'. $nao_finalizada .'</td>';
+                echo '<tr bgcolor="'. $color .'">';
+				if($disciplina_aluno['total_distribuido'] > 0)
+				{
+					echo '<td><a href="lista_notas_detalhe.php?c='. $curso .'&p='. $periodo .'&d='. $disciplina_aluno['ref_disciplina_ofer'] .'" alt="Clique para detalhar a disciplina" title="Clique para detalhar a disciplina">'. $disciplina_aluno['descricao_disciplina'] .'</a>'. $nao_finalizada .'</td>';
+				}
+				else
+				{
+					echo '<td>'. $disciplina_aluno['descricao_disciplina'] . $nao_finalizada .'</td>';				
+				}
 				echo '<td align="center">'. number::numeric2decimal_br($disciplina_aluno['nota_final'],1) .'</td>';
 				echo '<td align="center">'. $disciplina_aluno['total_distribuido'] .'</td>';
+								
 				echo '<td align="center">'. $disciplina_aluno['num_faltas'] .'</td>';
-                echo '<td align="center">'. $situacao .'</td>';
+				
+				//Inici­o: Victor Ullisses Pugliese - 12:38 27/04/2012 - CARGA HORARIA;
+                $sql_carga_horaria = "SELECT get_carga_horaria_realizada(".$disciplina_aluno['ref_disciplina_ofer'].");";
+        		$ch_realizada = $conn->get_one($sql_carga_horaria);
+                
+                echo '<td align="center">'. number::numeric2decimal_br(@($disciplina_aluno['num_faltas'] * 100 / $ch_realizada),1) .'</td>';
+                //Fim
+                
+                if($disciplina_aluno['fl_finalizada']=="" || $disciplina_aluno['fl_finalizada']=="f")
+				{                
+					echo '<td align="center"> M </td>';
+					$situacao = "M";
+					$m++;
+				}
+                else
+                {
+                	if($disciplina_aluno['nota_final'] < 6 || ($disciplina_aluno['num_faltas'] * 100 / $ch_realizada) >= 25)
+					{
+						echo '<td align="center"> R </td>';
+						$situacao = "R";
+					}
+					else
+					{
+						echo '<td align="center"> A </td>';
+						$situacao = "A";
+					}
+				}
 				echo '</tr>';
 
 				$count++;
@@ -97,8 +125,10 @@ $rs_diarios_matriculados = count($rs_diarios);
     ?>
 </table>
 <br />
-(<strong>*</strong>) Disciplina com lan&ccedil;amentos em aberto, pass&iacute;vel de altera&ccedil;&otilde;es.
-<br /><br />
+<?php
+	if($m>0)
+		echo "(<strong>*</strong>) Disciplina com lançamentos em aberto, passí­vel de alterações.<br /><br />";
+?>
 <div align="left" style="font-size: 0.85em;">
     <h4>Legenda</h4>
     <strong>A</strong> - Aprovado<br />
@@ -109,9 +139,11 @@ $rs_diarios_matriculados = count($rs_diarios);
 <br />
 <font color="red">
 <strong>
-Existem disciplinas matriculadas n&atilde;o exibidas. <br />
-Estas disciplinas somente estaram dispon&iacute;veis quando o professor(a) iniciar o lan&ccedil;amento das notas. <br />
-Qualquer d&uacute;vida entre em contato com seu professor(a) ou com a coordena&ccedil;&atilde;o do curso. <br />
+Existem disciplinas matriculadas não exibidas. <br />
+Estas disciplinas somente estaram disponí­veis quando o professor(a) iniciar o 
+lançaamento das notas. <br />
+Qualquer dúvida entre em contato com seu professor(a) ou com a 
+coordenação do curso. <br />
 </strong>
 </font>
 <?php endif; ?>
